f769961e96f8acbfc7102c1a8a3a6dc0bdc98673
wuwei <496384622@qq.com>
2024-06-12 15:58:44 +0800

    niubi

--- .vuepress/extraSideBar.ts
+++ .vuepress/extraSideBar.ts
@@ -12,11 +12,11 @@ export default [
   },
   {
     title: "星球",
-    icon: "/xiaoqingnian-plus/icon/xingqiu.png",
+    icon: "./icon/xingqiu.png",
     popoverTitle:
       '<span style="font-size:0.8rem;font-weight:bold;"><span style="color:red;">保姆级实战项目教程</span>、编程学习指南、学习资源、求职指南、技术分享、编程交流</span>',
     popoverUrl:
-      "/xiaoqingnian-plus/qrcode-codenav.png",
+      "./qrcode-codenav.png",
     popoverDesc: "知识星球：编程导航",
   },
   {
